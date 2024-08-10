66d7c4b7b89e49404b3a8293349e00f67bcb398a
Sashrika <sashrikad@gmail.com>
2024-08-09 20:39:57 -0400

    url name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://sashrikad.github.io',
+  url: 'https://iamsashrika.link',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
