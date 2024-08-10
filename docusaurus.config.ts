2a3cf9941364c1f36268976fa595d84944fcf64c
Sashrika <sashrikad@gmail.com>
2024-08-09 20:13:27 -0400

    fixes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'sashrikad', // Usually your GitHub org/user name.
-  projectName: 'sashrikad.github.io', // Usually your repo name.
+  projectName: 'iamsashrika', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
