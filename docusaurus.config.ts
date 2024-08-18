b3ebabb6a89ca6b76e763c47f32e0e6febaea431
Sashrika <sashrikad@gmail.com>
2024-08-17 21:37:41 -0400

    social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -59,7 +59,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/IAMSASHRIKA.LINK.jpg',
     navbar: {
       title: 'Iamsashrika',
       logo: {
